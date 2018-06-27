5d22bcb90a9fffe59540b1ea00f4064fcc1eb2a0
WonHwiAhn <myroom123@bible.ac.kr>
2018-06-27 16:03:22 +0900

    fourth

--- src/test/java/com/boot/blog/SpringBoot1ApplicationTests.java
+++ src/test/java/com/boot/blog/SpringBoot1ApplicationTests.java
@@ -7,7 +7,7 @@ import org.springframework.test.context.junit4.SpringRunner;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
-public class NaverOauth02ApplicationTests {
+public class SpringBoot1ApplicationTests {
 
 	@Test
 	public void contextLoads() {
