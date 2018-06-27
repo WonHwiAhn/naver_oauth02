4d68a5048fe1817640510c9385578c65a1e5c9b4
WonHwiAhn <myroom123@bible.ac.kr>
2018-06-27 15:53:23 +0900

    first

---
+++ src/main/java/com/boot/blog/NaverOauth02Application.java
@@ -0,0 +1,12 @@
+package com.boot.blog;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NaverOauth02Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NaverOauth02Application.class, args);
+	}
+}
